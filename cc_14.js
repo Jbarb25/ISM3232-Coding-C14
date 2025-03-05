effe67733016a436a55455e4bec96ffdab0e59f4
Jbarb25 <barboza205@usf.edu>
2025-03-05 10:00:15 -0500

    Task 2 - Support Tickets Dynamic Addition
    Create a function that plugs in the customer name, a description of the issue, and the priority level into the html display that is linked to each support ticket. Each ticket has a resolve button that removes the specific ticket indicating that the support ticket is no longer open. Each support ticket is linked to the the ticket container.

---
+++ cc_14.js
@@ -0,0 +1,33 @@
+//Task 2: Adding Support Tickets Dynamically
+const tickContainerByID = document.getElementById("ticketContainer"); //pull the document by its ID
+const tickContainerByQuery = document.querySelector('#ticketContainer');  //pull the document by the query's first instance
+
+    //Function that creates an employee card
+function createSupportTicket(customerName, issueDes, priorityLevel){
+    const supportTicket = document.createElement('div'); //create a new element to represent the employee card that would be stored in the div container
+    supportTicket.setAttribute('class', 'support-ticket'); //set the class of the new element as employee card
+    supportTicket.setAttribute('id', 'supportTicket'); //set the id of the new element as employee card
+    
+    //updates the text for the card with the parameters plugged in when function called
+    supportTicket.innerHTML = `
+    <h3 class= "customer-name">Customer Name: ${customerName}</h3>
+    <p class= "issues-description">Issues Description: ${issueDes}</p>
+    <label for= "priority-level">Priority Level: ${priorityLevel}</label>
+    <button class = "resolve-ticket">Resolve</button>
+    `; 
+
+    //create resolve button that resolves the support ticket when pressed
+const resolveButton = supportTicket.querySelector('.resolve-ticket');
+
+resolveButton.addEventListener('click', (event) => {
+  event.stopPropagation(); //clicking the resolve button will not bubble up to the ticket container
+  tickContainerByID.removeChild(supportTicket); //remove the selected support ticket
+});
+
+tickContainerByID.appendChild(supportTicket); //add element support ticket to the dashboard and the ticketContainer
+}
+
+//Testing the functions
+createSupportTicket("Emma Dole", "Refund denied", "High")
+createSupportTicket("John Spear", "Wrong item delivered", "Moderate")
+createSupportTicket("Jake Little", "Not working", "Moderate")
