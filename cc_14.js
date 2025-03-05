576f742d417f83069d6d3ed4f89ea220108f32ed
Jbarb25 <barboza205@usf.edu>
2025-03-05 11:25:52 -0500

    Task 5 - Inline Editing for Support Tickets
    Creating a function that allows one to edit the text within the browser for the customer name, the issue description, and the priority level and save it. This is done by establishing an edit button or double clicking the support ticket to open the editing mode and a saving button that will replace the text and exit out of the edit mode.

--- cc_14.js
+++ cc_14.js
@@ -7,16 +7,20 @@ function createSupportTicket(customerName, issueDes, priorityLevel){
     const supportTicket = document.createElement('div'); //create a new element to represent the employee card that would be stored in the div container
     supportTicket.setAttribute('class', 'support-ticket'); //set the class of the new element as employee card
     supportTicket.setAttribute('id', 'supportTicket'); //set the id of the new element as employee card
-    supportTicket.setAttribute('data-priority', priorityLevel); //set the priority level as an attribute of each ticket
+    supportTicket.setAttribute('priority-level', priorityLevel); //set the priority level as an attribute of each ticket
 
 
     //updates the text for the card with the parameters plugged in when function called
     supportTicket.innerHTML = `
     <h3 class= "customer-name">Customer Name: ${customerName}</h3>
     <p class= "issues-description">Issues Description: ${issueDes}</p>
-    <label for= "priority-level">Priority Level: ${priorityLevel}</label>
+    <label class= "priority-level">Priority Level: ${priorityLevel}</label>
     <button class = "resolve-ticket">Resolve</button>
+    <button class = "edit-ticket">Edit</button>
     `; 
+
+
+    tickContainerByID.appendChild(supportTicket); //add element support ticket to the dashboard and the ticketContainer
 
     //create resolve button that resolves the support ticket when pressed
 const resolveButton = supportTicket.querySelector('.resolve-ticket');
@@ -27,8 +31,51 @@ resolveButton.addEventListener('click', (event) => {
   tickContainerByID.removeChild(supportTicket); //remove the selected support ticket
 });
 
-tickContainerByID.appendChild(supportTicket); //add element support ticket to the dashboard and the ticketContainer
-}
+
+//Task 5: Additional Challenge - Inline Editing of Employee Details
+const editButton = supportTicket.querySelector('.edit-ticket'); //create edit button that will edit the employee info
+editButton.addEventListener('click', (event) => {
+    event.stopPropagation(); //stop it from bubbling to the employeeCard container
+    editSupportTicket(supportTicket);});
+
+supportTicket.addEventListener('dblclick', (event) => { //when employee card is double clicked, you may edit the employee info
+    event.stopPropagation();
+    editSupportTicket(supportTicket);});}
+
+function editSupportTicket(supportTicket){  //for the edit support ticket command, do the following to the support ticket info
+    const nameElement = supportTicket.querySelector('.customer-name'); //establish name element from the ticket
+    const issuesElement = supportTicket.querySelector('.issues-description'); //establish position element from the ticket
+    const priorityElement = supportTicket.querySelector('.priority-level');
+
+    const nameInput = document.createElement('input'); //make it so that name can be recognized as an input
+    nameInput.value = nameElement.innerText.replace('Customer Name: ', ''); //replace the inner text of the ticket
+    const issuesInput = document.createElement('input'); //make it so that issues description can be recognized as an input
+    issuesInput.value = issuesElement.innerText.replace('Issues Description: ', ''); //replace the inner text of the ticket
+    const priorityInput = document.createElement('input'); //make it so that priority level can be recognized as an input
+    priorityInput.value = priorityElement.innerText.replace('Priority Level: ', ''); //replace the inner text of the ticket
+
+
+    const saveButton = document.createElement('button'); //establish a save button to exit out of edit mode
+    saveButton.innerText = 'Save'; //save button is labeled as save
+
+    nameElement.innerHTML= ''; //allow the name element to be printed to the browser using the input
+    issuesElement.innerHTML= ''; //allow the issues description element to be printed to the browser using the input
+    priorityElement.innerHTML= ''; //allow the priority element to be printed to the browser using the input
+    nameElement.appendChild(nameInput); //link the edited input to the printed element
+    issuesElement.appendChild(issuesInput); //link the edited input to the printed element
+    priorityElement.appendChild(priorityInput); //link the edited input to the printed element
+
+    supportTicket.appendChild(saveButton); //link the save button to the support ticket
+
+    saveButton.addEventListener('click', () => {  //when the save button is clicked, do the following
+        nameElement.innerHTML = `Customer Name: ${nameInput.value}`; //print the edited name element such as
+        issuesElement.innerHTML = `Issues Description: ${issuesInput.value}`; //print the edited position element such as
+        priorityElement.innerHTML = `Priority Level: ${priorityInput.value}`; //print the edited position element such as
+
+        saveButton.remove(); //once the button is clicked, the button will disappear from the browser
+
+        const editButton = supportTicket.querySelector('.edit-ticket'); //the edit button will reappear as an option again
+        editButton.innerText = 'Edit';});}//edit button labeled as edit
 
 //Testing the functions
 createSupportTicket("Emma Dole", "Refund denied", "High")
@@ -37,7 +84,7 @@ createSupportTicket("Jake Little", "Not working", "Moderate")
 
 
 //Task 3: Converting NodeLists to Arrays for Bulk Updates
-const nodeList = document.querySelectorAll('.support-ticket[data-priority="High"]'); //create an element that stores all queries associated with support-ticket
+const nodeList = document.querySelectorAll('.support-ticket[priority-level="High"]'); //create an element that stores all queries associated with support-ticket
 const arrayOfNodes = Array.from(nodeList); //for the items in the nodelist element, convert to an array
 arrayOfNodes.forEach( item => {  //for every item in the array of nodes, do the following
         item.style.color = "#800000";  //change the text color to maroon in the printed html file
@@ -52,4 +99,9 @@ arrayOfNodes.forEach( item => {  //for every item in the array of nodes, do the 
     
    ticketContainer.addEventListener('click', () => { 
        console.log(`Ticket clicked`);});  //add an event listner so that when any ticket is clicked, it logs the message
+
+
+//Task 5: Additional Challenge - Inline Editing of Support Tickets 
+    //Refer to task 2 for the functions and variables created to make it so you can save and edit the text in the browser
+  
 
